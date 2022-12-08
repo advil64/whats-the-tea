a6e3627b25557305fdc7a603a0e8c574c47cc169
Advith Chegu <advil64@gmail.com>
2022-12-08 17:53:27 -0500

    Add files via upload

--- whats_the_tea/deep_learning_clustering/classify.py
+++ whats_the_tea/deep_learning_clustering/classify.py
@@ -1,21 +1,58 @@
-import findspark
-findspark.init()
+import json
+import torch
+import pathlib
+import argparse
+import numpy as np
+from time import time
+from tqdm import tqdm
+# from transformers import T5Tokenizer, T5ForConditionalGeneration
+from sentence_transformers import SentenceTransformer, util
 
-from pyspark.ml.feature import Word2Vec
-from pyspark import SparkConf, SparkContext
-from pyspark.sql import SQLContext
+LABELS = ['arts', 'arts & culture', 'black voices', 'business', 'college', 
+        'comedy', 'crime', 'culture & arts', 'divorce', 'education', 'entertainment', 
+        'environment', 'fifty', 'food & drink', 'good news', 'green', 
+        'healthy living', 'home & living', 'impact', 'latino voices', 
+        'media', 'money', 'parenting', 'parents', 'politics', 'queer voices', 
+        'religion', 'science', 'sports', 'style', 'style & beauty', 'taste', 
+        'tech', 'the worldpost', 'travel', 'u.s. news', 'weddings', 'weird news', 
+        'wellness', 'women', 'world news', 'worldpost']
 
-from pyspark import SparkContext
-from pyspark.streaming import StreamingContext #Import streaming context
-from pyspark.sql import SparkSession
+# device = torch.device('cuda')
 
-import spacy_sentence_bert
-from pyspark.sql import functions as F
+# "part-00860-1d603278-60b7-4e2d-980f-bc3a1287f222-c000.json"
 
-spark = SparkSession.builder\
-    .master('local[*]')\
-    .appName('explore')\
-    .getOrCreate()
-sc = spark.sparkContext._conf.setAll([('spark.driver.maxResultSize', '8g')])
+PREFIX = pathlib.Path("/common/users/shared/cs543_fall22_group3/combined/combined_raw")
 
-df = spark.read.json('/common/users/shared/cs543_fall22_group3/combined/deep_vectors')
+parser = argparse.ArgumentParser()
+parser.add_argument('--infile', type=pathlib.Path, required=True)
+parser.add_argument('--outpath', type=pathlib.Path, required=True)
+args = parser.parse_args()
+
+inpath = PREFIX / args.infile
+outfile = args.outpath / args.infile
+
+t0 = time()
+
+with open(inpath, 'r') as f:
+    data = f.readlines()
+
+print(f"Size of data = {len(data)}.")
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+choices = torch.load('choices.pt')
+
+queries = [json.loads(sample)['selected_text'] for sample in tqdm(data)]
+
+query_embedding = model.encode(queries, convert_to_tensor=True)
+# print(query_embedding.shape)
+
+cos_sims = util.cos_sim(query_embedding, choices)
+max_idxs = torch.argmax(cos_sims, dim=1)
+
+# print(max_idxs)
+assigned_labels = [LABELS[x] for x in max_idxs]
+
+with open(outfile, 'w') as f:
+    json.dump([{'query': q, 'label': l} for q, l in zip(queries, assigned_labels)], f)
+
+print(f"Done. Took {round(time() - t0, 5)} s.")
