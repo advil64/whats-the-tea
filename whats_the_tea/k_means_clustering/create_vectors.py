254ba2be533cfa247c65cf65cf8de7b3ea053fb3
Vipul Gharde <vipul.gharde@gmail.com>
2023-05-28 08:56:41 -0400

    Clean Up Code

--- whats_the_tea/k_means_clustering/create_vectors.py
+++ whats_the_tea/k_means_clustering/create_vectors.py
@@ -1,51 +1,55 @@
-# %%
-import findspark
-findspark.init()
-
 from pyspark.ml.feature import Word2Vec, Word2VecModel
-from pyspark.sql import functions as F
-import numpy as np
 from pyspark.ml.linalg import Vectors
-
-from pyspark import SparkContext
-from pyspark.streaming import StreamingContext #Import streaming context
 from pyspark.sql import SparkSession
+import findspark
 
-# %%
-spark = SparkSession.builder\
-    .master('local[*]')\
-    .appName('word2vec')\
+# Initialize SparkSession
+findspark.init()
+spark = SparkSession.builder \
+    .master('local[*]') \
+    .appName('word2vec') \
+    .config('spark.driver.maxResultSize', '8g') \
     .getOrCreate()
-sc = spark.sparkContext._conf.setAll([('spark.driver.maxResultSize', '8g')])
 
-# %%
-model = Word2VecModel.load('/common/users/shared/cs543_fall22_group3/models/word2vec')
+# Load Word2Vec model and vector table
+model_path = '/common/users/shared/cs543_fall22_group3/models/word2vec'
+model = Word2VecModel.load(model_path)
 vector_table = model.getVectors()
 
-# %%
+# Read processed dataframe
 processed_df = spark.read.json('/common/users/shared/cs543_fall22_group3/combined/combined_processed')
 
-# %%
-processed_df.drop('selected_text').coalesce(1).write.format('text').option('header', 'false').mode('overwrite').save('/common/users/shared/cs543_fall22_group3/combined/tokens.txt')
+# Write tokens to text file
+tokens_output_path = '/common/users/shared/cs543_fall22_group3/combined/tokens.txt'
+processed_df.drop('selected_text') \
+    .coalesce(1) \
+    .write.format('text') \
+    .option('header', 'false') \
+    .mode('overwrite') \
+    .save(tokens_output_path)
 
-# %%
+# Process tokens and calculate vectors
+vectors_output_path = '/common/users/shared/cs543_fall22_group3/combined/combined_vectors'
 vectors_df = None
 count = 0
 
-for line in open('/common/users/shared/cs543_fall22_group3/combined/tokens/sentences.txt'):
+with open(tokens_output_path) as file:
+    for line in file:
+        if count > 1000:
+            break
+        count += 1
+        print(f'Finished calculating {count} vectors')
 
-    if count > 1000:
-        break
-    count += 1
-    print('Finished calculating {} vectors'.format(count))
-    
-    tokens = line.strip().split(',')
-    vector_list = vector_table.filter(vector_table.word.isin(tokens)).drop('word').collect()
-    if vectors_df is None:
-        vectors_df = spark.createDataFrame([(Vectors.dense([v[0][0] for v in vector_list]), 1.0)], ["features", "weighCol"])
-    else:
-        temp = spark.createDataFrame([(Vectors.dense([v[0][0] for v in vector_list]), 1.0)], ["features", "weighCol"])
-        vectors_df = vectors_df.union(temp)
+        tokens = line.strip().split(',')
+        filtered_vectors = vector_table.filter(vector_table.word.isin(tokens)).drop('word').collect()
+        features = Vectors.dense([v[0][0] for v in filtered_vectors])
+        weighCol = 1.0
+        temp_df = spark.createDataFrame([(features, weighCol)], ["features", "weighCol"])
 
-# %%
-vectors_df.write.mode("Overwrite").json('/common/users/shared/cs543_fall22_group3/combined/combined_vectors')
+        if vectors_df is None:
+            vectors_df = temp_df
+        else:
+            vectors_df = vectors_df.union(temp_df)
+
+# Write vectors to JSON
+vectors_df.write.mode('Overwrite').json('/common/users/shared/cs543_fall22_group3/combined/combined_vectors')
--- whats_the_tea/k_means_clustering/word2vec.py
+++ whats_the_tea/k_means_clustering/word2vec.py
@@ -1,30 +1,23 @@
-# %%
-import findspark
-findspark.init()
-
 from pyspark.ml.feature import Word2Vec
-from pyspark import SparkConf, SparkContext
-from pyspark.sql import SQLContext
-
-from pyspark import SparkContext
-from pyspark.streaming import StreamingContext #Import streaming context
 from pyspark.sql import SparkSession
+import findspark
 
-# %%
-spark = SparkSession.builder\
-    .master('local[*]')\
-    .appName('word2vec')\
+# Initialize Spark
+findspark.init()
+spark = SparkSession.builder \
+    .master('local[*]') \
+    .appName('word2vec') \
+    .config('spark.driver.maxResultSize', '8g') \
     .getOrCreate()
-sc = spark.sparkContext._conf.setAll([('spark.driver.maxResultSize', '8g')])
 
-# %%
+# Read JSON data
 df = spark.read.json('/common/users/shared/cs543_fall22_group3/combined/combined_processed')
 
-# %%
-word2vec = Word2Vec(vectorSize=1, seed=42, minCount=0, inputCol = 'cleaned_text', outputCol = 'output_vectors')
+# Configure Word2Vec
+word2vec = Word2Vec(vectorSize=1, seed=42, minCount=0, inputCol='cleaned_text', outputCol='output_vectors')
 
-# %%
+# Train Word2Vec model
 model = word2vec.fit(df)
 
-# %%
+# Save the trained model
 model.write().overwrite().save('/common/users/shared/cs543_fall22_group3/models/word2vec')
