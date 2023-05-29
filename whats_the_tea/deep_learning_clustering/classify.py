3610f3c293c70765b0f9589b1723cedf3cc9b732
Vipul Gharde <vipul.gharde@gmail.com>
2023-05-29 13:07:56 -0400

    Clean up code

--- whats_the_tea/deep_learning_clustering/classify.py
+++ whats_the_tea/deep_learning_clustering/classify.py
@@ -6,41 +6,58 @@ from sentence_transformers import SentenceTransformer, util
 from time import time
 from tqdm import tqdm
 
-with open('labels.json') as f:
-    labels = json.load(f)
-
-labels = [label.lower() for label in labels]
-
 PREFIX = pathlib.Path('/common/users/shared/cs543_fall22_group3/combined/combined_raw')
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--infile', type=pathlib.Path, required=True)
-parser.add_argument('--outpath', type=pathlib.Path, required=True)
-args = parser.parse_args()
 
-inpath = PREFIX / args.infile
-outfile = args.outpath / args.infile
+def load_labels(file_path):
+    with open(file_path) as f:
+        labels = json.load(f)
 
-t0 = time()
+    return [label.lower() for label in labels]
 
-with open(inpath, 'r') as f:
-    data = f.readlines()
 
-print(f'Size of data = {len(data)}.')
+def load_articles(file_path):
+    with open(file_path) as f:
+        data = f.readlines()
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
-choices = torch.load('choices.pt')
+    return [json.loads(sample)['selected_text'] for sample in tqdm(data)]
 
-queries = [json.loads(sample)['selected_text'] for sample in tqdm(data)]
 
-query_embedding = model.encode(queries, convert_to_tensor=True)
+def save_results(file_path, articles, assigned_labels):
+    with open(file_path, 'w') as f:
+        json.dump([{'article': article, 'label': label} for article, label in zip(articles, assigned_labels)], f)
 
-cos_sims = util.cos_sim(query_embedding, choices)
-max_idxs = torch.argmax(cos_sims, dim=1)
 
-assigned_labels = [labels[x] for x in max_idxs]
+def main(args):
+    inpath = PREFIX / args.infile
+    outfile = args.outpath / args.infile
 
-with open(outfile, 'w') as f:
-    json.dump([{'query': q, 'label': l} for q, l in zip(queries, assigned_labels)], f)
+    t0 = time()
 
-print(f'Done. Took {round(time() - t0, 5)} s.')
+    labels = load_labels('labels.json')
+    print(f'Loaded {len(labels)} labels.')
+
+    articles = load_articles(inpath)
+    print(f'Loaded {len(articles)} articles.')
+
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    label_embeddings = torch.stack([model.encode(x, convert_to_tensor=True) for x in labels])
+    article_embeddings = model.encode(articles, convert_to_tensor=True)
+
+    cos_sims = util.cos_sim(article_embeddings, label_embeddings)
+    max_idxs = torch.argmax(cos_sims, dim=1)
+    assigned_labels = [labels[x] for x in max_idxs]
+
+    save_results(outfile, articles, assigned_labels)
+    print(f'Results saved to {outfile}.')
+
+    print(f'Done. Took {round(time() - t0, 5)} s.')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--infile', type=pathlib.Path, required=True)
+    parser.add_argument('--outpath', type=pathlib.Path, required=True)
+    args = parser.parse_args()
+
+    main(args)
