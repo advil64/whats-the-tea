fcc0e7aed45d608a13d7570c31b8d319b87198dc
Vipul Gharde <vipul.gharde@gmail.com>
2023-05-29 19:29:49 -0400

    Clean up code

--- deep_learning_clustering/model.py
+++ deep_learning_clustering/model.py
@@ -3,34 +3,31 @@ import torch.nn as nn
 
 
 class TweetClassifier(nn.Module):
-    def __init__(self, embed_dim=300, out_channels=64, kernel_size=3, stride=1, padding=0, p=0.25, num_classes=42):
+    def __init__(self, out_channels=64, kernel_size=3, p=0.25, num_classes=42):
         super(TweetClassifier, self).__init__()
-        self.embed_dim = embed_dim
         self.out_channels = out_channels
         self.kernel_size = kernel_size
-        self.stride = stride
-        self.padding = padding
         self.p = p
         self.num_classes = num_classes
 
-        self.conv = nn.Conv1d(self.embed_dim, self.out_channels, self.kernel_size, self.stride, self.padding)
-        self.relu = nn.ReLU()
-        self.max_pool = nn.MaxPool1d(self.kernel_size, self.stride)
+        self.conv1 = nn.Conv1d(1, self.out_channels, self.kernel_size)
+        self.relu1 = nn.ReLU()
+        self.max_pool1 = nn.MaxPool1d(self.kernel_size)
+        self.conv2 = nn.Conv1d(self.out_channels, self.out_channels, self.kernel_size)
+        self.relu2 = nn.ReLU()
+        self.max_pool2 = nn.MaxPool1d(self.kernel_size)
         self.dropout = nn.Dropout(self.p)
-        self.fc = nn.Linear(self._linear_layer_in_size(), self.num_classes)
-
-    def _linear_layer_in_size(self):
-        conv_out_dim = (self.embed_dim + 2 * self.padding - self.kernel_size) // self.stride + 1
-        pool_out_dim = (conv_out_dim - self.kernel_size) // self.stride + 1
-
-        return pool_out_dim * self.out_channels
+        self.fc = nn.Linear(32 * self.out_channels, self.num_classes)
 
     def forward(self, x):
-        x = self.conv(x)
-        x = self.relu(x)
-        x = self.max_pool(x)
-        x = x.flatten(start_dim=1)
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.max_pool1(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.max_pool2(x)
         x = self.dropout(x)
+        x = x.flatten(start_dim=1)
         x = self.fc(x)
 
         return x
